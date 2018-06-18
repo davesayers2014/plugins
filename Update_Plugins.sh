ecfc428a4467a17a78385af1a2e6214e2affe437
davesayers2014 <davesayers2012@gmail.com>
2018-06-18 13:13:51 +0100

    Update Update_Plugins.sh

--- Update_Plugins.sh
+++ Update_Plugins.sh
@@ -1,8 +1,15 @@
 #! /bin/bash -
 echo "Updating core files please wait"
 opkg update >/dev/null 2>&1
+echo $LINE
 echo "Reinstalling AutoBouquetsMaker please wait"
+echo $LINE
 opkg install -force-reinstall enigma2-plugin-systemplugins-autobouquetsmaker
+echo $LINE
+echo $LINE
 echo "Reinstalling CrossEPG"
+echo $LINE
 opkg install -force-reinstall enigma2-plugin-systemplugins-crossepg
+echo $LINE
+echo $LINE
 echo "Plugins Updated please reboot box"
