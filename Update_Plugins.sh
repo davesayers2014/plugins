caaf4ad88df8ffe3d698b448d8a22d231c6ebb30
davesayers2014 <davesayers2012@gmail.com>
2018-11-03 09:35:22 +0000

    Update Update_Plugins.sh

--- Update_Plugins.sh
+++ Update_Plugins.sh
@@ -6,5 +6,6 @@ echo "Reinstalling AutoBouquetsMaker please wait"
 echo $LINE
 opkg install -force-reinstall enigma2-plugin-systemplugins-autobouquetsmaker
 echo $LINE
+mkdir -p /hdd/images/flash > dev/null 2>&1
 echo $LINE
 echo "Plugins Updated please reboot box"
