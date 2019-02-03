028aa582d6222a2833e6bb4edbf1c088f1d807b6
davesayers2014 <davesayers2012@gmail.com>
2019-02-03 18:16:47 +0000

    Add to new line in case a user already has a m3u

--- iptv.sh
+++ iptv.sh
@@ -2,4 +2,4 @@ read -p "Enter the username for your IPTV  : " username
 read -p "Enter the password for your IPTV : " password
 read -p "Enter the url your provider gave you for your IPTV  : " iptvhost
 
-echo  "$iptvhost/get.php?username=$username&password=P$password&type=m3u_plus&output=ts" > /etc/enigma2/jediplaylists/playlists.txt
+echo  "$iptvhost/get.php?username=$username&password=P$password&type=m3u_plus&output=ts" >> /etc/enigma2/jediplaylists/playlists.txt
