069788801e2d3e4419f398f601e2780795062103
davesayers2014 <davesayers2012@gmail.com>
2019-05-12 14:55:01 +0100

    Update Update_Plugins.sh
    Add e2iplayer updater

--- Update_Plugins.sh
+++ Update_Plugins.sh
@@ -7,5 +7,16 @@ echo $LINE
 opkg install -force-reinstall enigma2-plugin-systemplugins-autobouquetsmaker
 echo $LINE
 mkdir -p /hdd/images/flash >/dev/null 2>&1
+echo "Reinstalling E2iplayer"
+rm -rf e2iplayer-master
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
+cd /tmp 
+wget https://gitlab.com/e2i/e2iplayer/-/archive/master/e2iplayer-master.zip
+unzip e2iplayer-master.zip
+rm e2iplayer-master.zip
+cp -r e2iplayer-master/IPTVPlayer /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
+rm -rf e2iplayer-master
+
+sync
 echo $LINE
 echo "Plugins Updated please reboot box"
