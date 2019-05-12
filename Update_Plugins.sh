90bd05a02377132d4df91c8ca02895367750cc5d
davesayers2014 <davesayers2012@gmail.com>
2019-05-12 18:50:12 +0100

    Update Update_Plugins.sh

--- Update_Plugins.sh
+++ Update_Plugins.sh
@@ -13,7 +13,7 @@ rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
 cd /tmp 
 wget https://gitlab.com/e2i/e2iplayer/-/archive/master/e2iplayer-master.zip
 unzip e2iplayer-master.zip
-rm e2iplayer-master.zip
+rm /tmp/e2iplayer-master.zip
 cp -r e2iplayer-master/IPTVPlayer /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
 rm -rf e2iplayer-master
 
