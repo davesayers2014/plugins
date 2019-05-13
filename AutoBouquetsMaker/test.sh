0bf41daf62a74387e9522d236750842587ed35f9
davesayers2014 <davesayers2012@gmail.com>
2019-05-13 20:16:29 +0100

    Update test.sh

--- AutoBouquetsMaker/test.sh
+++ AutoBouquetsMaker/test.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+echo $LINE
+echo "Reinstalling E2iplayer"
+rm -rf e2iplayer-master
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
+cd /tmp 
+wget https://gitlab.com/e2i/e2iplayer/-/archive/master/e2iplayer-master.zip
+unzip e2iplayer-master.zip
+rm /tmp/e2iplayer-master.zip
+cp -r e2iplayer-master/IPTVPlayer /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
+rm -rf e2iplayer-master
+
+sync
+echo $LINE
+echo "Plugins Updated please reboot box"
 echo "Checking and installing AutoBouquetsMaker "
 echo "*****************************************"
 echo $LINE
@@ -9,25 +23,11 @@ if [ -e /usr/lib/enigma2/python/BoxBrandingTest.pyo ]; then
 		echo "**********************************"
 		opkg install https://github.com/davesayers2014/plugins/blob/master/AutoBouquetsMaker/enigma2-plugin-systemplugins-autobouquetsmaker_3.1+git594+b6397ce-r0_mips32el.ipk?raw=true
 		echo "*********************************"
-		echo "                                  "
+		echo "                                 "
+		echo "Plugins Updated please reboot box"
 else
 	echo "                                  "
 	echo "**********************************"
 	opkg install https://github.com/davesayers2014/plugins/blob/master/AutoBouquetsMaker/enigma2-plugin-systemplugins-autobouquetsmaker_3.1+git594+b6397ce-r0_cortexa15hf-neon-vfpv4.ipk?raw=true
 	echo "*********************************"
-	then
-	echo $LINE
-	echo "Reinstalling E2iplayer"
-	rm -rf e2iplayer-master
-	rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
-	cd /tmp 
-	wget https://gitlab.com/e2i/e2iplayer/-/archive/master/e2iplayer-master.zip
-	unzip e2iplayer-master.zip
-	rm /tmp/e2iplayer-master.zip
-	cp -r e2iplayer-master/IPTVPlayer /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
-	rm -rf e2iplayer-master
-
-	sync
-	echo $LINE
 	echo "Plugins Updated please reboot box"
-exit 0
