76ead6d93ee6652a767f25afae6565c0375a504e
davesayers2014 <davesayers2012@gmail.com>
2019-05-13 20:20:57 +0100

    Update test.sh

--- AutoBouquetsMaker/test.sh
+++ AutoBouquetsMaker/test.sh
@@ -12,10 +12,9 @@ rm -rf e2iplayer-master
 
 sync
 echo $LINE
-echo "Plugins Updated please reboot box"
+echo $LINE
 echo "Checking and installing AutoBouquetsMaker "
 echo "*****************************************"
-echo $LINE
 if [ -e /usr/lib/enigma2/python/BoxBrandingTest.pyo ]; then 
 	python /usr/lib/enigma2/python/BoxBrandingTest.pyo | sed 's/<$//' | sed 's/ /_/g' > /tmp/boxbranding.cfg
 	if grep -qs 'getImageArch=mips32el' cat /tmp/boxbranding.cfg  ; then
@@ -23,11 +22,17 @@ if [ -e /usr/lib/enigma2/python/BoxBrandingTest.pyo ]; then
 		echo "**********************************"
 		opkg install https://github.com/davesayers2014/plugins/blob/master/AutoBouquetsMaker/enigma2-plugin-systemplugins-autobouquetsmaker_3.1+git594+b6397ce-r0_mips32el.ipk?raw=true
 		echo "*********************************"
-		echo "                                 "
-		echo "Plugins Updated please reboot box"
+		echo "                                  "
+		exit 1
+	fi
 else
 	echo "                                  "
 	echo "**********************************"
 	opkg install https://github.com/davesayers2014/plugins/blob/master/AutoBouquetsMaker/enigma2-plugin-systemplugins-autobouquetsmaker_3.1+git594+b6397ce-r0_cortexa15hf-neon-vfpv4.ipk?raw=true
 	echo "*********************************"
+	echo "                                  "
+	exit 1
+fi
+
+exit 0
 	echo "Plugins Updated please reboot box"
