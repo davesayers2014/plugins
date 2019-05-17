1c8f242d335d6c401c2e796a9a61c4b21bab12cc
davesayers2014 <davesayers2012@gmail.com>
2019-05-17 17:52:33 +0100

    Update Update_Plugins.sh

--- Update_Plugins.sh
+++ Update_Plugins.sh
@@ -1,12 +1,5 @@
-#! /bin/bash -
-echo "Updating core files please wait"
-opkg update >/dev/null 2>&1
+#!/bin/sh
 echo $LINE
-echo "Reinstalling AutoBouquetsMaker please wait"
-echo $LINE
-opkg install -force-reinstall enigma2-plugin-systemplugins-autobouquetsmaker
-echo $LINE
-mkdir -p /hdd/images/flash >/dev/null 2>&1
 echo "Reinstalling E2iplayer"
 rm -rf e2iplayer-master
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
@@ -19,4 +12,27 @@ rm -rf e2iplayer-master
 
 sync
 echo $LINE
-echo "Plugins Updated please reboot box"
+echo $LINE
+echo "Checking and installing AutoBouquetsMaker "
+echo "*****************************************"
+if [ -e /usr/lib/enigma2/python/BoxBrandingTest.pyo ]; then 
+	python /usr/lib/enigma2/python/BoxBrandingTest.pyo | sed 's/<$//' | sed 's/ /_/g' > /tmp/boxbranding.cfg
+	if grep -qs 'getImageArch=mips32el' cat /tmp/boxbranding.cfg  ; then
+		echo "                                  "
+		echo "**********************************"
+		opkg install https://github.com/davesayers2014/plugins/blob/master/AutoBouquetsMaker/enigma2-plugin-systemplugins-autobouquetsmaker_3.1+git594+b6397ce-r0_mips32el.ipk?raw=true
+		echo "*********************************"
+		echo "                                  "
+		exit 1
+	fi
+else
+	echo "                                  "
+	echo "**********************************"
+	opkg install https://github.com/davesayers2014/plugins/blob/master/AutoBouquetsMaker/enigma2-plugin-systemplugins-autobouquetsmaker_3.1+git594+b6397ce-r0_cortexa15hf-neon-vfpv4.ipk?raw=true
+	echo "*********************************"
+	echo "                                  "
+	#exit 1
+fi
+
+exit 0
+	echo "Plugins Updated please reboot box"
