a1dd0ba7da5b45c110a6de4bb898977166a22eea
davesayers2014 <davesayers2012@gmail.com>
2019-06-10 17:07:26 +0100

    Update lamedb.sh

--- Slyk7dayFiles/lamedb.sh
+++ Slyk7dayFiles/lamedb.sh
@@ -1,1 +1,29 @@
+#!/bin/sh
+echo "##########################################################"
+echo "Downloading Slyk7Day file"
+echo "##########################################################"
+echo "Please wait till script complets"
+echo "##########################################################"
 
+echo $LINE
+echo $LINE
+echo "#########################################################"
+echo "Downloading lamedb"
+echo "#########################################################"
+echo $LINE
+echo $LINE
+
+# Download lamedb
+wget -O /etc/enigma2/slykepg7day/lamedb "https://raw.githubusercontent.com/davesayers2014/plugins/master/Slyk7dayFiles/lamedb"
+echo $LINE
+echo $LINE
+echo "#########################################################"
+echo "Downloading virgin-replacements.txt"
+echo "#########################################################"
+# Download lamedb
+wget -O /etc/enigma2/slykepg7day/virgin-replacements.txt "https://raw.githubusercontent.com/davesayers2014/plugins/master/Slyk7dayFiles/virgin-replacements.txt"
+echo $LINE
+echo $LINE
+echo "#########################################################"
+echo "Files Downloaded exit and run slyk-epg-7-day-downloader"
+echo "#########################################################"
