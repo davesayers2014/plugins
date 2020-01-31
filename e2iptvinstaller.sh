10074f46fa4379e1871a06c9b6ad59e5f897de7b
davesayers2014 <davesayers2012@gmail.com>
2020-01-31 17:29:25 +0000

    Update e2iptvinstaller.sh

--- e2iptvinstaller.sh
+++ e2iptvinstaller.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-# remove old version
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
-
-# Download and install plugin
-cd /tmp 
-set -e
-wget "https://www.softrix.co.uk/istream/downloads/e2istream_pythonX.X.tar.gz"
-
-tar -xzf e2istream_pythonX.X.tar.gz -C /
-set +e
-rm -f e2istream_pythonX.X.tar.gz
-cd ..
-
-sync
 echo "#########################################################"
-echo "#   e2iStream INSTALLED SUCCESSFULLY please reboot box  #"
+echo "#      ERROR  e2iStream install is currently offline #"
 echo "#########################################################"
 reboot
