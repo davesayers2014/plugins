fecb42cbb2eb7c0107474eefb2ffaaf68cb90f84
davesayers2014 <davesayers2012@gmail.com>
2020-01-31 17:31:37 +0000

    Update Update_Plugins.sh

--- Update_Plugins.sh
+++ Update_Plugins.sh
@@ -1,35 +1,9 @@
 #!/bin/sh
-echo "##########################################################"
-echo "# 		E2tools plugin script			"
-echo "##########################################################"
-echo "##########################################################"
-echo "#		Please wait till script complets		"
-echo "##########################################################"
 
-echo $LINE
-echo $LINE
-echo "#########################################################"
-echo "#               Installing e2iStream            	       "
-echo "#########################################################"
-echo $LINE
-echo $LINE
-
-# remove old version
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer
-
-# Download and install plugin
-cd /tmp 
-set -e
-wget "https://www.softrix.co.uk/istream/downloads/e2istream_pythonX.X.tar.gz"
-
-tar -xzf e2istream_pythonX.X.tar.gz -C /
-set +e
-rm -f e2istream_pythonX.X.tar.gz
-cd ..
 
 sync
 echo "#########################################################"
-echo "#          e2iStream INSTALLED SUCCESSFULLY             #"
+echo "#          e2iStream INSTALL offline to be fixed           #"
 echo "#########################################################"
 
 #sync
