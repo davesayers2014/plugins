9f3fb1a34a290375b7ccda7b3a7303c3a9680832
davesayers2014 <davesayers2012@gmail.com>
2020-05-16 18:18:49 +0100

    Update xstreamity.sh

--- xstreamity.sh
+++ xstreamity.sh
@@ -5,20 +5,32 @@ cd /tmp
 
 unset LD_PRELOAD
 
-echo "\n XStreamity setup script"
+echo "XStreamity setup script"
+
+opkg install python-image
+
+opkg install python-imaging
+
+opkg install python-argparse
+
+opkg install python-requests
+
+opkg install python-multiprocessing
+
+
 
 echo "downloading file"
 
 wget -O /tmp/XStreamity-master.zip https://github.com/kiddac/XStreamity/archive/master.zip
   
-echo "\n  unzipping file"
+echo "unzipping file"
 unzip -oq /tmp/XStreamity-master.zip
   
 cp -pr /tmp/XStreamity-master/XStreamity/usr/lib/enigma2/python/* /usr/lib/enigma2/python/
 
 rm -rf /tmp/XStreamity-master /tmp/XStreamity-master.zip
 
-echo "\n XStreamity playlist setup"
+echo XStreamity playlist setup"
 echo $LINE
 echo $LINE
 read -p "Enter the username for your IPTV  : " username
@@ -28,7 +40,7 @@ echo $LINE
 read -p "Enter the host url your provider gave you for your IPTV  : " iptvhost
 echo  "$iptvhost/get.php?username=$username&password=$password&type=m3u_plus&output=ts" >> /etc/enigma2/playlists.txt
 echo $LINE
-echo  "\n Writting settings please wait"
+echo  "Writting settings please wait"
 cd /etc/enigma2/
 cd .
 init 4
@@ -37,7 +49,7 @@ sed -i '$i config.plugins.XStreamity.location=/etc/enigma2/' settings
 init 3
 echo $LINE
 echo $LINE
-echo "\nRestarting GUI"
+echo "Restarting GUI"
 echo $LINE
 echo $LINE
-echo "\n Playlists.txt written goto XStreamity and choose server"
+echo "Playlists.txt written goto XStreamity and choose server"
